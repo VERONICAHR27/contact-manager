b2dd1845345d6560ddb948e7a8e748d67b333080
VERONICAHR27 <hilarioreyesveronica6@gmail.com>
2025-03-21 00:22:50 -0500

    lab08-localstore

--- src/pages/ContactListPage.jsx
+++ src/pages/ContactListPage.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
-const ContactListPage = () => {
+const ContactListPage = ({ contacts, onSyncContacts }) => {
+  // Función para guardar contactos en localStorage
+  const handleSaveContacts = () => {
+    if (contacts && contacts.length > 0) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+      alert('Contactos guardados en LocalStorage.');
+    } else {
+      alert('No hay contactos para guardar.');
+    }
+  };
+
   return (
     <div className="contact-list-page">
-      
+      <h1>Agenda de Contactos</h1>
+      <button onClick={handleSaveContacts}>Guardar Contactos</button> {/* Botón para guardar contactos */}
+      <button onClick={onSyncContacts}>Sincronizar Datos</button> {/* Botón para sincronizar datos */}
       <nav>
         <Link to="social">Social</Link> | <Link to="familia">Familia</Link> | <Link to="trabajo">Trabajo</Link>
       </nav>
